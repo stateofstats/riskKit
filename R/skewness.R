183cc3fee81d3d00083535206ddb4b19fad36d88
Derek Peterson <depeterson23@gmail.com>
2023-02-06 13:30:34 -0600

    commit skewness Rd

--- R/skewness.R
+++ R/skewness.R
@@ -1,3 +1,14 @@
+#' Skewness
+#'
+#' @param x A vector of data on which you wish to calculate the skewness
+#'
+#' @return A single skewness value
+#' @export
+#'
+#' @examples
+#' x <- df$values
+#' skewness(x)
+#'
 skewness <- function(x){
 
   demeaned_x <- x - mean(x)
