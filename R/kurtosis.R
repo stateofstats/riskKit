aefe2b7806f421aa65a60c8a71771161d652a505
Derek Peterson <depeterson23@gmail.com>
2023-02-06 13:40:57 -0600

    commit kurtosis Rd

--- R/kurtosis.R
+++ R/kurtosis.R
@@ -1,3 +1,14 @@
+#' Kurtosis
+#'
+#' @param x A vector of data on which you wish to calculate the kurtosis
+#'
+#' @return A single kurtosis value
+#' @export
+#'
+#' @examples
+#' x <- df$values
+#' kurtosis(x)
+#'
 kurtosis <- function(x){
 
   demeaned_x <- x - mean(x)
