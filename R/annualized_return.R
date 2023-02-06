f737aeee3dcbce2f58e9f7fc82bb702d00748d50
stateofstats <depeterson23@gmail.com>
2023-02-06 17:53:02 -0600

    commit annualized return function

---
+++ R/annualized_return.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param x A series of asset returns
+#' @param periods The number of periods in a year
+#'
+#' @return The compounded annualized return for a return series
+#' @export
+#'
+#' @examples
+annualize_returns <- function(x, periods){
+  compounded_growth <- prod(1 + x)
+  n_periods <- nrow(x)
+  return(compounded_growth ** (periods_per_year / n_periods) - 1)
+}
